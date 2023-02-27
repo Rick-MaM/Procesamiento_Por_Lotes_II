8ea016afc03532b2ec76eecbc1b81294d1ceec42
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-02-26 21:30:44 -0600

    Se modifico la funcion files_copy

--- Practica_2.py
+++ Practica_2.py
@@ -2,8 +2,6 @@ import os
 from random import randint
 
 Directory = os.getcwd() + "\Archivos"
-os.chdir(Directory)
-Conetnido = os.listdir()
 
 def random_number(minimum, maximum):
     return randint(minimum,maximum)
@@ -27,22 +25,60 @@ def identify(Contents):
             folder.append(count)
     return files, folder
 
-def files_copy(files):
-    for count_file in range(len(files)):
-        file_name = files[count_file]
-        text = read_file(file_name)
-        for count_char in range(len(text)):
-            letter = text[count_char]
-            if (ord(text[count_char]) >= 48 and ord(text[count_char]) <= 57):
-                letter = chr(random_number(65, 90))
-            elif ((ord(text[count_char]) >= 65 and ord(text[count_char]) <= 90) or (ord(text[count_char]) >= 97 and ord(text[count_char]) <= 122)):
-                letter = chr(random_number(48, 57))
-            create_file(letter, file_name)
+def files_copy(file):
+    text = read_file(file)
+    letter = ""
+    for count_char in range(len(text)):
+        
+        if (ord(text[count_char]) >= 48 and ord(text[count_char]) <= 57):
+            letter = letter + chr(random_number(65, 90))
+        elif ((ord(text[count_char]) >= 65 and ord(text[count_char]) <= 90) or (ord(text[count_char]) >= 97 and ord(text[count_char]) <= 122)):
+            letter = letter + chr(random_number(48, 57))
+        else:
+            letter = letter + text[count_char]
+    
+    return letter
+
+#----------------CARPETAS------------------
+def new_folder(directory):
+    list = directory.split("\\")
+    list.pop()
+    list = "\\".join(list)
+    os.chdir(list)
+    os.mkdir("Archivos_Copiados")
+    return list + "\Archivos_Copiados"
 
 
-txt, carp = identify(Conetnido)
-files_copy(txt)
-print("Listo jalisco")
+
+def folder_contents(directory):
+    os.chdir(directory)
+    contests = os.listdir()
+    file, folder = identify(contests)
+
+    copy_folder = new_folder(directory)
+    
+    
+
+    while len(file) != 0:
+        os.chdir(directory)
+        text = files_copy(file[0])
+        os.chdir(copy_folder)
+        create_file(text,file[0])
+        file.pop(0)
+    
+
+def create_folder_directory(name):
+    new_name = "Copia_"+name
+    os.mkdir(new_name)
+    return os.getcwd() + "/" + name, os.getcwd() + "/" + new_name
+
+
+def folder_copy(folder):
+    for count_folder in range(len(folder)):
+        origin,copy = create_folder_directory(folder[count_folder])
+        
+
+folder_contents(Directory)
 
             
 
